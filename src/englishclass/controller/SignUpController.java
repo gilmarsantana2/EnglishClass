e984545a9d0597b24e5c510a7c72f9a29a6438f5
gilmarsantana2 <gilmarsantana2@gmail.com>
2021-04-11 20:55:33 -0300

    adcionado tela de sign up

---
+++ src/englishclass/controller/SignUpController.java
@@ -0,0 +1,69 @@
+package englishclass.controller;
+
+import englishclass.model.ModelAcess;
+import englishclass.util.Animation;
+import englishclass.view.ViewFactory;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SignUpController extends AbstractController implements Initializable {
+
+    @FXML
+    private AnchorPane anchorPane;
+    @FXML
+    private TextField txtUsername;
+    @FXML
+    private ImageView userImage;
+    @FXML
+    private PasswordField txtPassword;
+    @FXML
+    private PasswordField txtConfPassword;
+    @FXML
+    private TextField txtname;
+
+    public SignUpController(ModelAcess model) {
+        super(model);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        new Animation(anchorPane).fadeInUp();
+    }
+
+    @FXML
+    public void goAddImage(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void goCancel(ActionEvent event) {
+        LoginController controller = getModel().getController();
+        var anima = new Animation(anchorPane).fadeOutDown();
+        anima.setOnFinished(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                controller.stackPane.getChildren().remove(1);
+                controller.changeAttributes(false);
+            }
+        });
+        anima.play();
+    }
+
+    @FXML
+    public void goSignUp(ActionEvent event) {
+
+    }
+
+
+}
